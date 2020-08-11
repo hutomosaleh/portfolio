313857fd6fe983b0997df293887137dd4e0a92f2
hutomosaleh <61884180+hutomosaleh@users.noreply.github.com>
2020-08-11 21:57:14 +0200

    Update toggle.js

--- js/toggle.js
+++ js/toggle.js
@@ -1,62 +1,83 @@
-var home = document.querySelector('#home');
-var about = document.querySelector('#about');
-var active = '#home'
+// var home = document.querySelector('#home');
+// var about = document.querySelector('#about');
+// var active = '#home';
 
+// home.onclick = function () {
+//   show('#home');
+// };
+// about.onclick = function () {
+//   show('#about');
+// };
 
-home.onclick=function () {
-     show("#home");
-}
-about.onclick=function () {
-     show("#about");
-}
+// function show(page) {
+//   if (page != active) {
+//     console.log(active);
+//     asideSectionTogglerBtn();
+//     // Handles highlighting
+//     var choice = document.querySelector(page);
+//     choice.classList.add('active'); // highlights chosen page
+//     var old_choice = document.querySelector(active);
+//     old_choice.classList.remove('active'); // un-highlights old page
 
-function show(page){
+//     active_temp = active + '-page';
+//     active = page; // saves current page information
 
-     if ( page != active) {
-          console.log(active)
-          asideSectionTogglerBtn();
-          // Handles highlighting
-          var choice = document.querySelector(page);
-          choice.classList.add("active");  // highlights chosen page
-          var old_choice = document.querySelector(active);
-          old_choice.classList.remove("active");  // un-highlights old page
-     
-          active_temp = active + "-page"
-          active = page  // saves current page information
-     
-          // Handles show page
-          page = page + "-page"
-          var choice = document.querySelector(page);
-          choice.classList.remove("hidden");  // shows chosen page
-          var old_choice = document.querySelector(active_temp);
-          old_choice.classList.add("hidden");  // hides old page
-          
-          console.log(active)
-     }
-}
+//     // Handles show page
+//     page = page + '-page';
+//     var choice = document.querySelector(page);
+//     choice.classList.remove('hidden'); // shows chosen page
+//     var old_choice = document.querySelector(active_temp);
+//     old_choice.classList.add('hidden'); // hides old page
 
-// var navBar = document.querySelector(".nav").children;
-// var navBarlength = navBar.length;
-
-// for (let i = 0; i < navBarlength; i++) {
-//      navBar[i].addEventListener("click", function() {
-//           console.log(this.children[0].id);
-//           var choice = this.children[0];
-//           choice.classList.add("active");
-//           active = choice.id
-//           choice.classList.add("active");
-//      })
+//     console.log(active);
+//   }
 // }
 
-const navTogglerBtn = document.querySelector(".nav-toggler");
-const aside = document.querySelector(".aside");
+// Revised version of navbar toggle
+const main = document.querySelector('.main-content').children;
+const navbar = document.querySelector('.nav').children;
+const navbar_length = navbar.length;
+var active_navbar = 0;
 
-navTogglerBtn.addEventListener("click", () => {
-     asideSectionTogglerBtn();
-})
+for (let i = 0; i < navbar_length; i++) {
+  navbar[i].addEventListener('click', function () {
+    if (i != active_navbar) {
+      navbar[active_navbar].children[0].classList.toggle('active');
+      navbar[i].children[0].classList.toggle('active');
+      main[active_navbar].classList.toggle('hidden');
+      main[i].classList.toggle('hidden');
+      active_navbar = i;
+    }
+  });
+}
+
+const pmenu = document.querySelector('.project-menu').children;
+const pmenu_length = pmenu.length;
+const pgroup = document.querySelector('.project-group').children;
+var active_project = 1;
+
+// Changes the project menu highlight and shows/hides relevant projects
+for (let i = 0; i < pmenu_length; i++) {
+  pmenu[i].addEventListener('click', function () {
+    if (i != active_project) {
+      pmenu[active_project].children[0].classList.toggle('active');
+      pmenu[i].children[0].classList.toggle('active');
+      pgroup[active_project].classList.toggle('hidden');
+      pgroup[i].classList.toggle('hidden');
+      active_project = i;
+    }
+  });
+}
+
+const navTogglerBtn = document.querySelector('.nav-toggler');
+const aside = document.querySelector('.aside');
+
+navTogglerBtn.addEventListener('click', () => {
+  asideSectionTogglerBtn();
+});
 
 // Toggles the navbar
 function asideSectionTogglerBtn() {
-     aside.classList.toggle("open");
-     navTogglerBtn.classList.toggle("open");
+  aside.classList.toggle('open');
+  navTogglerBtn.classList.toggle('open');
 }
