feab5c493c6797993c13aae2fe70cf5af93e3999
hutomosaleh <61884180+hutomosaleh@users.noreply.github.com>
2020-08-12 13:19:12 +0200

    Update styleswitcher.js

--- js/styleswitcher.js
+++ js/styleswitcher.js
@@ -12,7 +12,7 @@ function setActiveStyle(color) {
      }
 }
 
-// Body Skin
+// Skin color changer: detects changes from radio button
 const bodySkin = document.querySelectorAll(".body-skin");
 const totalBodySkin = bodySkin.length;
 
@@ -20,7 +20,7 @@ for (let i = 0; i < totalBodySkin; i++) {
      bodySkin[i].addEventListener("change", function() {
           console.log(this.value);
           if (this.value === "dark") {
-               // document.body.classList.add("dark")
+               // document.body.classList.add("dark") | Alternative
                document.body.className = "dark";
           }
           else {
@@ -29,9 +29,29 @@ for (let i = 0; i < totalBodySkin; i++) {
      })
 }
 
+const styleswitcher = document.querySelector(".style-switcher");
+
+// Won't detect the navbar opening, somehow
+// // swiped-left
+// document.addEventListener('swiped-left', function(e) {
+//      // if styles is closed AND navbar is CLOSED, we can swipe it open
+//      if (aside.classList.contains('open') != true && styleswitcher.classList.contains('open') != true) {
+//           styleswitcher.classList.toggle("open");
+//           console.log(aside.classList.contains('open'));
+//      };
+// });
+
+//    // swiped-right
+// document.addEventListener('swiped-right', function(e) {
+//      // if styles is open AND navbar is CLOSED, we can swipe it closed
+//      if (styleswitcher.classList.contains('open') != false && aside.classList.contains('open') !=  true) {
+//           styleswitcher.classList.toggle("open");
+//      }
+// });
+
 document.querySelector(".toggle-style-switcher").addEventListener("click", () => {
      console.log("Yo this button is working");
-     document.querySelector(".style-switcher").classList.toggle("open");
+     styleswitcher.classList.toggle("open");
 })
 
 
